238fc4d6fa52e5285b87b47c292202bf97863445
ps29653 <SharmaPrashant@JohnDeere.com>
2020-12-07 08:45:20 +0530

    Stack implementation using LinkedList

---
+++ src/MyStack.java
@@ -0,0 +1,71 @@
+package src;
+
+public class MyStack {
+
+    Node currentNode;
+
+    Node previousNode;
+
+    int length;
+
+    public MyStack(int value) {
+
+        this.currentNode = new Node(value);
+        this.previousNode = null;
+        length++;
+
+    }
+
+    public void push(int newValue) {
+
+        Node newNode = new Node(newValue);
+        this.previousNode = this.currentNode;
+        this.currentNode = newNode;
+        this.currentNode.nextNode = this.previousNode;
+        length++;
+
+    }
+
+    public void pop(){
+
+        this.currentNode = this.previousNode;
+        this.previousNode = currentNode.nextNode;
+        length--;
+
+    }
+    public int peek(){
+
+        return this.currentNode.value;
+    }
+
+    public void printValues() {
+
+        Node currentNode = this.currentNode;
+        while (currentNode.nextNode != null){
+            System.out.print(currentNode.value + ", ");
+            currentNode = currentNode.nextNode;
+        }
+        System.out.println(currentNode.value);
+
+    }
+
+    public int getLength(){
+
+        return this.length;
+    }
+
+
+    static class Node {
+
+        Node(int value) {
+
+            this.value = value;
+            this.nextNode = null;
+        }
+
+        int value;
+
+        Node nextNode;
+    }
+
+}
