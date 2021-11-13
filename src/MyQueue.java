a1f77bfee1033af701a72d20808d8b3cbb13fbd0
Prashant Sharma <prashant.sharma1thoughtworks.com>
2021-11-13 19:21:44 +0530

    own-impl-stack-and-queue

--- src/MyQueue.java
+++ src/MyQueue.java
@@ -9,47 +9,38 @@ public class MyQueue {
     private int length;
 
     public void enqueue(int newValue) {
-
         Node newNode = new Node(newValue);
-        if (this.length == 0) {
+        if (this.length == 0)
             this.first = newNode;
-            this.last = newNode;
-        } else {
+        else
             this.last.next = newNode;
-            this.last = newNode;
-        }
+
+        this.last = newNode;
         this.length++;
     }
 
     public void peek() {
-        if (this.length == 0) {
+        if (this.length == 0)
             System.out.println("Empty Queue");
-        } else {
+        else
             System.out.println(this.first.value);
-        }
     }
 
     public void dequeue() {
-
-        if (this.length == 0) {
+        if (this.length == 0)
             System.out.println("Empty Queue");
-        }
-        if (length == 1) {
+        if (length == 1)
             this.last = null;
-        }
         this.first = this.first.next;
         this.length--;
     }
 
 
     static class Node {
-
         int value;
-
         Node next;
 
         public Node(int newValue) {
-
             this.value = newValue;
             this.next = null;
         }
--- src/MyStack.java
+++ src/MyStack.java
@@ -12,25 +12,20 @@ public class MyStack {
     int length;
 
     public MyStack(int value) {
-
         this.currentNode = new Node(value);
         this.previousNode = this.currentNode;
         length++;
-
     }
 
     public void push(int newValue) {
-
         Node newNode = new Node(newValue);
         this.previousNode = this.currentNode;
         this.currentNode = newNode;
         this.currentNode.nextNode = this.previousNode;
         length++;
-
     }
 
     public void pop() {
-
         if (length == 1) {
             this.previousNode = null;
             this.currentNode = null;
@@ -39,24 +34,20 @@ public class MyStack {
             this.previousNode = currentNode.nextNode;
         }
         length--;
-
     }
 
     public void peek() {
-
-        if(this.currentNode == null){
+        if (this.currentNode == null)
             System.out.println("No item in Stack");
-        }else {
+        else
             System.out.println(this.currentNode.value);
-        }
-
     }
 
     public void printValues() {
 
-        if (length == 0) {
+        if (length == 0)
             System.out.println("Empty Stack");
-        } else {
+        else {
             Node currentNode = this.currentNode;
             while (currentNode.nextNode != null) {
                 System.out.print(currentNode.value + ", ");
@@ -64,25 +55,19 @@ public class MyStack {
             }
             System.out.println(currentNode.value);
         }
-
     }
 
     public int getLength() {
-
         return this.length;
     }
 
-
     static class Node {
-
         Node(int value) {
-
             this.value = value;
             this.nextNode = null;
         }
 
         int value;
-
         Node nextNode;
     }
 
